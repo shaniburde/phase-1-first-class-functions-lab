df40a5222a4e24ffe2529bfe8be26dfd4e8d7fac
shani <shhhaniburde@gmail.com>
2021-12-14 13:31:32 -0500

    commit tree a99e2b84a07211675b1092f65801aed8f91df566

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (x) => x.slice(0, 2);
+
+const returnLastTwoDrivers = (x) => x.slice(2,4);
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Claudia'];
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (x) => {
+    return function(fare) {
+        return x * fare
+    };
+};
